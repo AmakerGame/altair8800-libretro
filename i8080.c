17fb572e283d0f4ffb278bbfb19c4f4d61b529f4
Edytor-Studio Core <volosovecadrian@gmail.com>
2026-04-24 10:36:01 +0300

    Create i8080.c

---
+++ i8080.c
@@ -0,0 +1,20 @@
+#include "i8080.h"
+
+void i8080_reset(i8080* cpu) {
+    cpu->pc = 0;
+    cpu->sp = 0;
+    for(int i = 0; i < 65536; i++) cpu->memory[i] = 0;
+}
+
+void i8080_step(i8080* cpu) {
+    uint8_t opcode = cpu->memory[cpu->pc];
+    cpu->pc++;
+    switch(opcode) {
+        case 0x00: break; 
+        case 0xC3: {
+            uint16_t addr = cpu->memory[cpu->pc] | (cpu->memory[cpu->pc+1] << 8);
+            cpu->pc = addr;
+            break;
+        }
+    }
+}
